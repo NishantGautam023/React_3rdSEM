dca18d2959747119b0bf27f0ddbdbeb9a0e4acdd
NishantGautam023 <gautamniishant@icloud.com>
2022-08-23 17:40:48 -0400

    Activity 2.1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react-x8xlpf
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-x8xlpf)
+
+Solved Exercises from React.js – Key Concepts By Maximilian Schwarzmüller
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello this is Nishant Gautam!</h1>
+      <p>
+        Right now, I am solving exercises from React.js – Key Concepts By
+        Maximilian Schwarzmüller
+      </p>
     </div>
   );
 }
